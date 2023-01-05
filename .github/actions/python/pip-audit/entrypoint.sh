bd67cb42191f90cf5da011a7477a8ab4683a3ef2
Björn Wikström <bjorn@ourstudio.se>
2023-01-05 13:43:08 +0100

    Set threshold to `high` for Snyk scanners

--- .github/actions/python/pip-audit/entrypoint.sh
+++ .github/actions/python/pip-audit/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pip-audit -l $@
+REQUIREMENTS_FILE=""
+
+while (( "$#" )); do
+    case $1 in
+        --requirements-file)
+            shift && REQUIREMENTS_FILE="${1}"
+            ;;
+    esac
+
+    shift || break
+done
+
+pip-audit -l -r ${REQUIREMENTS_FILE}
